325330cb5f5f3a99c92232b7046f168bc86cbc14
JewelSama <osafilejewel@gmail.com>
2024-04-19 14:39:16 +0100

    home

--- Components/Menu.tsx
+++ Components/Menu.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { baseURL } from '../endpoints'
 // import { Feather } from '@expo/vector-icons'
 
-const Menu = () => {
+const Menu = ({ menu }: any) => {
   return (
     <View  className={`bg-white border px-4 pt-4 pb-2 border-gray-200`} style={{marginBottom: 2}}>
         <View className="flex-row">
             <View className="flex-1 pr-2">
-                <Text className="text-lg mb-1" style={{fontFamily: 'SemiBold'}}>{"Jollof Rice"}</Text>
-                <Text className="text-slate-700" style={{fontFamily: 'Regular'}}>{"Sweet and tasty Jollof"}</Text>
-                <Text className="text-slate-700 mt-2" style={{fontFamily: 'Regular'}}> ₦{"1500"}</Text>
+                <Text className="text-lg mb-1" style={{fontFamily: 'SemiBold'}}>{menu?.name}</Text>
+                <Text className="text-slate-700" style={{fontFamily: 'Regular'}}>{menu?.description}</Text>
+                <Text className="text-slate-700 mt-2" style={{fontFamily: 'Regular'}}> ₦{menu?.price}</Text>
             </View>
             <View>
                 <Image 
                     style={{borderWidth: 1, borderColor: "#f3f3f4"}}
-                    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAEAtfryy3h79PfpxPs7zB6hNVslMDtTu5S2AdP8wqjQ&s'}}
+                    source={{uri: baseURL+'/'+menu?.display_pic}}
                     className="h-20 w-20 bg-gray-300 p-4"
                 />
             </View>
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,21 +1,77 @@
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Menu from '../Components/Menu'
+import { AppContext } from '../Providers/AppProvider'
+import { VendorMenusAPI } from '../endpoints'
 
 const HomeScreen = ({ navigation }: any) => {
+  const { user } = useContext<any>(AppContext)
+  const [ loading, setLoading ] = useState(false)
+  const [ menus, setMenus ] = useState([])
+  // console.log("user ", user.id)
+
+  const getMenu = async() => {
+    setLoading(true)
+    await fetch(`${VendorMenusAPI}/${user?.id}`, {
+      method: 'GET',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${user?.token}`
+      })
+    })
+    .then(res => res.json())
+    .then(resp => {
+      setLoading(false)
+      if(resp?.errors){
+        return alert(resp?.message)
+      }
+      // console.log(resp)
+      setMenus(resp?.data.reverse()); 
+    })
+    .catch(err => {
+      setLoading(false)
+      console.log(err)
+      alert('Something went wrong')
+    })
+  }
+
+  useEffect(() => {
+    getMenu()
+  }, [])
+
+
   return (
     <SafeAreaView className='h-full' style={{paddingHorizontal: 6}}>
     <View className='h-full'>
         <View className='pb-2  items-center mt-1 border-gray-200' style={{borderBottomWidth: 1}}>
-            <Text className="text-xl" style={{fontFamily: 'Bold'}}>Chillout Restaurant Menu</Text>
+            <Text className="text-xl" style={{fontFamily: 'Bold'}}>{user?.name} Menu</Text>
         </View>
 
         <ScrollView className='h-full mt-4' showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100}}>
-            <>
-                <Menu />
-                <Menu />
-            </>
+        {
+                !loading ? (
+                    <View>
+                        {
+                            menus && menus.map((menu: any, index: any) => (
+                                // @ts-ignore
+                                <Menu menu={menu} key={index} />
+                            ))
+
+                        }{
+                          menus.length < 1 && (
+                            <View className='h-20 justify-center items-center'>
+                              <Text className='text-lg' style={{fontFamily: 'Regular'}}>You have no menu</Text>
+                            </View>
+                          )
+                        }
+                    </View>
+                ) : (
+                    <View className='w-full'>
+                      <ActivityIndicator color="#064929" size={'small'} />
+                    </View>
+                )
+            }
         </ScrollView>
     </View>
     </SafeAreaView>
