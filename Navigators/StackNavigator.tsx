705f567f3742052615d0c848136ea97d839a10ff
JewelSama <osafilejewel@gmail.com>
2024-04-19 14:39:37 +0100

    stack

--- Navigators/StackNavigator.tsx
+++ Navigators/StackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import App from '../App';
 import TabNavigator from './TabNavigator';
+import OrderDetailScreen from '../Screens/OrderDetailScreen';
 
 
 
@@ -11,6 +12,7 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator>
         <Stack.Screen name="TabNavigator" component={TabNavigator} options={{headerShown: false}} />
+        <Stack.Screen name="OrderDetail" component={OrderDetailScreen} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
