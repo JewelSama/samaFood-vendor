30b91f7196985facc998fa6f81523599bc60ca61
JewelSama <osafilejewel@gmail.com>
2024-04-19 12:48:18 +0100

    auth endpoints

---
+++ endpoints.ts
@@ -0,0 +1,10 @@
+export const baseURL = 'https://jewel.cmsandbox.com.ng'
+export const RegisterAPI = 'https://jewel.cmsandbox.com.ng/api/vendor/register'
+export const LoginAPI = 'https://jewel.cmsandbox.com.ng/api/vendor/login'
+// export const MenusAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/menu/all'
+// export const VendorsAPI = 'https://jewel.cmsandbox.com.ng/api/vendors/all'
+// export const CreateOrderAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/order/new'
+// export const GetOrderAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/orders'
+// export const VendorMenuAPI = 'https://jewel.cmsandbox.com.ng/api/vendor/menu'  // -> /{id}
+// export const UpdateProfileAPI = 'https://jewel.cmsandbox.com.ng/api/buyer/profile'  // -> /{id}
+// export const PaystackKey = 'pk_test_4e4d75a86f6c6eb34aa43963d0d6d2179d3d0594'
