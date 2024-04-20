f5821069bce74935def409eeaa1a7dec159c87c4
JewelSama <osafilejewel@gmail.com>
2024-04-20 13:13:18 +0100

    order

--- Components/Order.tsx
+++ Components/Order.tsx
@@ -24,7 +24,7 @@ const Order = ({ order }: any) => {
           )
         }
         {
-          order.status === "completed" && (
+          order.status === "accepted" && (
             <Text style={{fontFamily: 'Regular'}} className='text-green-500'>{order.status.toUpperCase()}</Text>
           )
         }
