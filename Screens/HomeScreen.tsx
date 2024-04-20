740bbe3dcfef0e82b46402a991217b5ad2d2c5f4
JewelSama <osafilejewel@gmail.com>
2024-04-20 13:13:26 +0100

    refresh contrl

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, ActivityIndicator, RefreshControl } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Menu from '../Components/Menu'
@@ -6,14 +6,15 @@ import { AppContext } from '../Providers/AppProvider'
 import { VendorMenusAPI } from '../endpoints'
 
 const HomeScreen = ({ navigation }: any) => {
-  const { user } = useContext<any>(AppContext)
+  const { user, menus, setMenus } = useContext<any>(AppContext)
   const [ loading, setLoading ] = useState(false)
-  const [ menus, setMenus ] = useState([])
+  const [refreshing, setRefreshing] = React.useState(false);
+
   // console.log("user ", user.id)
 
-  const getMenu = async() => {
+  const getMenu = async(id: string) => {
     setLoading(true)
-    await fetch(`${VendorMenusAPI}/${user?.id}`, {
+    await fetch(`${VendorMenusAPI}/${id}`, {
       method: 'GET',
       headers: new Headers({
         'Accept': 'application/json',
@@ -26,8 +27,8 @@ const HomeScreen = ({ navigation }: any) => {
       if(resp?.errors){
         return alert(resp?.message)
       }
-      // console.log(resp)
-      setMenus(resp?.data.reverse()); 
+      console.log(resp)
+      setMenus(resp.data); 
     })
     .catch(err => {
       setLoading(false)
@@ -36,19 +37,35 @@ const HomeScreen = ({ navigation }: any) => {
     })
   }
 
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+      getMenu(user?.id)
+    }, 2000);
+  }, []);
+
   useEffect(() => {
-    getMenu()
-  }, [])
+    setLoading(true)
+    setTimeout(() => {
+      console.log(122344)
+      console.log("userTimeout: ", user.id)
+      getMenu(user?.id)
+    }, 1000)
+    setLoading(false)
+  }, [user])
 
 
   return (
-    <SafeAreaView className='h-full' style={{paddingHorizontal: 6}}>
+    <SafeAreaView className='h-full bg-white' style={{paddingHorizontal: 6}}>
     <View className='h-full'>
         <View className='pb-2  items-center mt-1 border-gray-200' style={{borderBottomWidth: 1}}>
             <Text className="text-xl" style={{fontFamily: 'Bold'}}>{user?.name} Menu</Text>
         </View>
 
-        <ScrollView className='h-full mt-4' showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100}}>
+        <ScrollView className='h-full mt-4' showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100}} refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
         {
                 !loading ? (
                     <View>
@@ -59,7 +76,7 @@ const HomeScreen = ({ navigation }: any) => {
                             ))
 
                         }{
-                          menus.length < 1 && (
+                          menus?.length < 1 && (
                             <View className='h-20 justify-center items-center'>
                               <Text className='text-lg' style={{fontFamily: 'Regular'}}>You have no menu</Text>
                             </View>
